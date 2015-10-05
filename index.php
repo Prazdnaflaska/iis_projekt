6e0a653519e479b070b8033de2cece0dab0d6177
cerv0077 <cervenkaondra@gmail.com>
2015-10-05 12:10:06 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+<head>
+	<title>Rezervace letenek</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+		<div id="header">
+		<h1>Rezervace letenek</h1>
+
+		</div>
+		<div id="menu">
+		 <nav>
+            <ul class="fancyNav">
+                <li id="home"><a href="index.php" class="homeIcon">Úvod</a></li>
+                <li id="news"><a href="#news">Rezervace</a></li>
+                <li id="about"><a href="login.php">Login</a></li>
+                <li id="services"><a href="#services">Registrace</a></li>
+            </ul>
+        </nav>
+        </div>
+        <div id="textField">
+        <p>Tento informační systém vznikl z důvodu školního projektu, a takové podobné pičoviny kurva 
+        hovna srakčy</p>
+        </div>	
+</body>
+</html>
