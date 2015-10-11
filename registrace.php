c142ccdcde1d7cd9730a6c3540a86193e05bc6e7
filija <filija.jakub@gmail.com>
2015-10-11 15:14:49 +0200

    DOkonceni registrace

--- registrace.php
+++ registrace.php
@@ -1,3 +1,95 @@
+<?php
+	session_start();
+
+	$empty=false; //Overovani vyplneni formulare
+	$uspech=true; //Pozadavky pro registraci
+
+ if(!empty($_POST))
+ {
+	foreach ($_POST as $argument) 
+	{
+		if(empty($argument))
+		{
+			$empty=true;
+			break;
+		}	
+	}
+
+	if($empty)
+	{	
+		header("location: registrace.php?info=Vyplňte prosím všechny údaje !!!");
+		$uspech=false;
+	}
+
+	else{
+		/*pripojeni do databaze*/
+		$link=mysql_connect("127.0.0.1", "root", ""); //!!!Nezapomente zadat jako 3. parametr svoje heslo
+
+		if(!$link)
+		{
+			echo "Chyba spojeni s databazi";
+			exit(1);
+		}
+
+		/*Vyber databaze*/
+		$select=mysql_select_db("rezervace_letenek", $link);
+
+		if(!$select)
+		{
+			echo "Neni mozne vybrat databazi";
+			exit(1);
+		}
+
+		$jmena=mysql_query("SELECT login FROM uzivatele", $link);
+
+		/*Kontrola, zda zadany login existuje*/
+	  while ($Kontrola_loginu=mysql_fetch_row($jmena))
+	  {
+	  	foreach ($Kontrola_loginu as $login)
+		{
+			
+			if($login==$_POST["mail"] && $uspech)
+			{
+				header("location: registrace.php?info=tento login nebo E-mail již existuje !!!");
+				$uspech=false;
+			}
+		}
+	  }
+		
+		/*Kontrola hesla*/
+		if($_POST["pass_reg"]!=$_POST["pass_reg2"] && $uspech)
+		{
+			header("location: registrace.php?info=hesla se neshodují !!!");
+			$uspech=false;
+		}
+
+		/*delka hesla, alespon 8 znaku*/
+		else if((strlen($_POST["pass_reg"])<=8) && $uspech)
+		{
+			header("location: registrace.php?info=Heslo je příliš krátké, musí obsahovat alespoň 8 znaků !!!");
+			$uspech=false;
+		}
+
+		/*Pokud vsechny udaje byly spravne, uloz do databaze*/
+		if($uspech)
+		{	
+			$vlozeni=mysql_query("INSERT INTO uzivatele(login, heslo, jmeno, prijmeni, adresa, email, telefon, is_admin) 
+			VALUES ('$_POST[mail]','$_POST[pass_reg]','$_POST[jmeno]','$_POST[prijmeni]','$_POST[add]','$_POST[mail]','$_POST[tel]', false)", $link);	
+			if($vlozeni)
+			{
+				echo "Uspesna registrace ".$Kontrola_loginu[0];	
+				header("location: registrace.php?info=Registrace proběhla úspěšně & correct=true");
+				exit();
+			}
+
+			else{
+				echo "nepodarilo se vas vlozit do databaze, zkuste to prosim znovu";
+			}
+		}
+	}
+}	
+?>
+
 <!DOCTYPE html>
 <html>
 <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
@@ -37,12 +129,26 @@
             </ul>
         </nav>
         </div>
+         <div id="infopanel"><br>
+         <?php
+         	if(!empty($_SESSION['username']))
+         	{
+         		echo "Jste přihlášen jako ". htmlspecialchars($_SESSION['username']);
+         		echo "<br>";
+        		echo "<a href=\"login.php?odhlasit\">Odhlásit</a>";
+        	}
+         	else
+         		echo "Nejste přihlášen";
+         ?>
+         </div>
         <div id="pageField">
             <div id="reg_form">
                 <div id="login">
                     <h2>Registrace</h2>
                 </div>
                 <div id="jmeno_prij">
+                
+                <form method="post">
                 <input id="jmeno" name="jmeno" placeholder="Jméno" type="text">
                 <input id="prijmeni" name="prijmeni" placeholder="Příjmení" type="text">
                  <input id="add" name="add" placeholder="Adresa" type="text">
@@ -55,7 +161,20 @@
                 <input id="pass_reg2" name="pass_reg2" placeholder="Kontrla hesla" type="password">
 
 
-            <input name="submit_reg" type="submit_reg" value=" Přihlásit ">
+            <input name="submit_reg" type="submit" value=" Registrovat ">
+
+            </form>
+            	 <?php 
+            	 	/*Vysledek registrace, popripade chyba*/
+            	 	if($_GET['correct'])
+            	 	{	
+            	 		echo "<h3 style=\"color: green; font-weight: bold;\">".$_GET['info']."</h3>";
+            	 		echo "<a href=\"index.php\" style=\"font-size: 20px; color: blue;\">Přejít na hlavní stránku</a>";
+            	  	}
+
+            	 	else		            	 			
+            	 		echo "<h3 style=\"color: red; font-weight: bold;\">".$_GET['info']."</h3>";
+                ?>
                 </div>
                 
 
