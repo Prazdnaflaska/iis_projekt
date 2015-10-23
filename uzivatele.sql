87777211491199a9291349aadd8ff517071cb5ba
kuba <filija.jakub@gmail.com>
2015-10-23 13:42:02 +0200

    drobne upravy

--- uzivatele.sql
+++ uzivatele.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Databáze: `rezervace_letenek`
+-- Databáze: `flaskadb`
 --
 
 -- --------------------------------------------------------
