ecf780897853608216f6bfd2fa56998420e366fe
kuba <filija.jakub@gmail.com>
2015-10-23 10:02:02 +0200

    Vytvoreni editace

--- admin.php
+++ admin.php
@@ -1,5 +1,7 @@
 <?php
 	session_start();
+	require_once('editace.php');
+
   if(!$_SESSION['admin'])
   { 
     echo "<meta charset=\"utf-8\">";
@@ -7,23 +9,29 @@
     echo "<a href=\"index.php\" style=\"font-size: 25px;\">zpět na hlavní stránku</a>";
      exit();
   } 
+
   if(!isset($_SESSION['username']))
   {
   	header('location: login.php');
     exit();
   }
+
   if($_GET['admin'])
   {
   		$_SESSION['admin']=true;
   }
+
   if(isset($_GET['odhlasit']))
   {
     session_destroy();
     header('location: index.php');
     exit();
   }
+
 	$empty=false; //Overovani vyplneni formulare
 	$uspech=true; //Pozadavky pro registraci
+
+
  if(!empty($_POST))
  {
 	foreach ($_POST as $argument) 
@@ -34,27 +42,19 @@
 			break;
 		}	
 	}
+
 	if($empty)
 	{	
 		header("location: admin.php?info=Vyplňte prosím všechny údaje !!!");
 		$uspech=false;
 	}
+
 	else{
 		/*pripojeni do databaze*/
-		$link=mysql_connect("127.0.0.1", "root", "webcam"); //!!!Nezapomente zadat jako 3. parametr svoje heslo
-		if(!$link)
-		{
-			echo "Chyba spojeni s databazi";
-			exit(1);
-		}
-		/*Vyber databaze*/
-		$select=mysql_select_db("rezervace_letenek", $link);
-		if(!$select)
-		{
-			echo "Neni mozne vybrat databazi";
-			exit(1);
-		}
+		$link=getConnectDb();
+
 		$jmena=mysql_query("SELECT login FROM uzivatele", $link);
+
 		/*Kontrola, zda zadany login existuje*/
 	  while ($Kontrola_loginu=mysql_fetch_row($jmena))
 	  {
@@ -75,12 +75,14 @@
 			header("location: admin.php?info=hesla se neshodují !!!");
 			$uspech=false;
 		}
+
 		/*delka hesla, alespon 8 znaku*/
 		else if((strlen($_POST["pass_reg"])<=8) && $uspech)
 		{
 			header("location: admin.php?info=Heslo je příliš krátké, musí obsahovat alespoň 8 znaků !!!");
 			$uspech=false;
 		}
+
 		/*Pokud vsechny udaje byly spravne, uloz do databaze*/
 		if($uspech)
 		{	
@@ -92,6 +94,7 @@
 				header("location: admin.php?info=Registrace proběhla úspěšně & correct=true");
 				exit();
 			}
+
 			else{
 				echo "nepodarilo se vas vlozit do databaze, zkuste to prosim znovu";
 			}
@@ -111,32 +114,27 @@
 	<head>
 		<title>Rezervace letenek</title>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 		<script>
   			$(function() {
     			$( "#datepicker2" ).datepicker();
   			});
-
+  		</script>
+  		<script>
   			$(function() {
     			$( "#datepicker" ).datepicker();
   			});
-
-  			function ref()
-  			{
-  				$('#show_div').load('form.php');
-  			}
-  			
-
+		</script>
+		<script type="text/javascript">
 			function showDiv(){
 				document.querySelector('#main').classList.add('blur');
 				document.querySelector('#show_div').classList.remove('hidden');
 			}
-
-			
 			
 		</script>
+
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
@@ -145,8 +143,25 @@
                 <div id="login">
                     <h2>Registrace</h2>
                 </div>
-                <?php include 'form.php';?>
-        </div>
+                <div id="jmeno_prij">
+                
+                	<form method="post">
+                		<input id="jmeno" name="jmeno" placeholder="Jméno" type="text">
+                		<input id="prijmeni" name="prijmeni" placeholder="Příjmení" type="text">
+                 		<input id="add" name="add" placeholder="Adresa" type="text">
+                 		<input id="tel" name="tel" placeholder="Telefon" type="text">
+                 		<input id="mail" name="mail" placeholder="E-mail" type="text">
+                </div>
+                <div id="jmeno_prij">
+                	<input id="pass_reg" name="pass_reg" placeholder="Heslo" type="password">
+                	<input id="pass_reg2" name="pass_reg2" placeholder="Kontrla hesla" type="password">
+            		<input name="submit_reg" type="submit" value=" Registrovat ">
+            	
+                </div>
+                </form>
+                	
+                	<a class="material-icons" href="admin.php">cancel</a>
+                </div>	
         </div>
 
  		<div id="main">
@@ -160,7 +175,7 @@
                 		<li id="news"><a href="admin.php">Akce</a></li>
                 		<?php 
               			if($_SESSION['admin'])
-              				echo "<li id=\"admin\"><a href=\"admin.php\">Administrace</a></li>";
+              				echo "<li id=\"admin\"><a href=\"admin.php\">administrace</a></li>";
               			?>
                 		<li id="services"><a href="registrace.php">Registrace</a></li>
             		</ul>
@@ -170,104 +185,68 @@
         		<?php
         			if($_SESSION['admin'])
         				echo 'admin';
+
         		?>
         		<br>
         		<a href="login.php?odhlasit">Odhlásit</a>
         
         	</div>
         	<div id="pageField">
-        	
         		<div id="textField_users">
-
-        				
         			<div id="administrace">
        					<h2>Users</h2>
        				</div>
-       				
-       				<table class="data">
-       				<tr class="head">
-       					<td>Jméno</td>
-       					<td>Přijmení</td>
-       					<td>Adresa</td>
-       					<td>E-mail</td>
-       					<td>Telefon</td>
-       					<td>Admin</td>
-       					<td></td>
-       					<td></td>
-
-       				</tr>
+       				<table>
+       				<form method="post" action="editace.php">
        					<?php 
-       						mysql_connect("127.0.0.1","root","webcam");
+       						mysql_connect("127.0.0.1","root","decathlon");
        						mysql_select_db("rezervace_letenek");
-       						$result=mysql_query("SELECT jmeno, prijmeni, adresa, email, telefon, is_admin FROM uzivatele");
+       						$result=mysql_query("SELECT jmeno, prijmeni, adresa, email, telefon, login FROM uzivatele");
+							$x=0;
+							$y=0;
 							while($row = mysql_fetch_row($result))
 								{
 									echo "<tr>";
-									echo "<td>$row[0] </td>";
-    								echo "<td>$row[1] </td>";
-    								echo "<td>$row[2] </td>";
-    								echo "<td>$row[3] </td>";
-    								echo "<td>$row[4] </td>";
-    								echo "<td>$row[5] </td>";
-    								echo "<td><input type=\"button\" value=\"Upravit\" onclick=\"window.close()\"></td>";
-    								echo "<td><input type=\"button\" value=\"Odstranit\" onclick=\"window.close()\"></td><br>";
+									echo "<td><input name=\"jmeno[$y]\" type=\"text\" value=\"$row[0]\"></td>";
+    								echo "<td><input name=\"prijmeni[$y]\"type=\"text\" value=\"$row[1]\"></td>";
+    								echo "<td><input name=\"adresa[$y]\" type=\"text\" value=\"$row[2]\"> </td>";
+    								echo "<td><input name=\"email[$y]\" type=\"text\" value=\"$row[3]\"></td>";
+    								echo "<td><input name=\"telefon[$y]\" type=\"text\" value=\"$row[4]\"> </td>";
+    								echo "<td><input name=\"login[$y]\" type=\"hidden\" value=\"$row[5]\"> </td>";
+    								echo "<td><input type=\"submit\" value=\"Upravit\" name=\"submit[$x]\"></td>";
+    								echo "<td><input type=\"submit\" value=\"Odstranit\" name=\"delete[$x]\"></td><br>";
     								echo "</tr>\n";
+    								$x++;
+    								$y++;	
+
     							}
     					?>
-       				</table>
-       			</div>
-<div id="tlacitko">
-        				<form method="post">
-        				<table class="edit_menu">
+       				</table>       				
+        				<?php
+        						echo "<h3 style=\"color: green; font-size: 20px; font-weight: bold;\">".$_GET['stav']."</h3>";
         				
-        					<tr><td><input type="button" name="reg_window" value="Registrace uživatele" onclick="location.href='registrace.php'"></td></tr>
-        					<tr><td><input type="button" name="reg_windows" value="Vytvořit rezervaci" onclick="hello()"></td></tr>
-        					<tr><td><input type="button" name="reg_windows" value="Vytvořit let" onclick="hello()"></td></tr>
-        					<tr><td><input type="button" name="reg_windows" value="Přidat společnost" onclick="hello()"></td></tr>
-        					
-        					
-        					
-        					</table>
-        				</form>
-        			</div>	
+		            	 	/*Vysledek registrace, popripade chyba*/
+        		    	 	if($_GET['correct'])
+            		 		{	
+            	 				echo "<h3 style=\"color: green; font-weight: bold; font-size: 20px;\">".$_GET['info']."</h3>";
+            	 				
+            	  			}
 
-       			<div id="textField_users">
-
-        				
-        			<div id="administrace">
-       					<h2>Rezervace</h2>
-       				</div>
-       				
-       				<table class="data">
-       					<?php 
-       						mysql_connect("127.0.0.1","root","webcam");
-       						mysql_select_db("rezervace_letenek");
-       						$result=mysql_query("SELECT jmeno, prijmeni, adresa, email, telefon FROM uzivatele");
-							while($row = mysql_fetch_row($result))
-								{
-									echo "<tr>";
-									echo "<td>$row[0] </td>";
-    								echo "<td>$row[1] </td>";
-    								echo "<td>$row[2] </td>";
-    								echo "<td>$row[3] </td>";
-    								echo "<td>$row[4] </td>";
-    								echo "<td><input type=\"button\" value=\"Upravit\" onclick=\"window.close()\"></td>";
-    								echo "<td><input type=\"button\" value=\"Odstranit\" onclick=\"window.close()\"></td><br>";
-    								echo "</tr>\n";
-    							}
-    					?>
-       				</table>
+            	 			else		            	 			
+            	 				echo "<h3 style=\"color: red; font-weight: bold; font-size: 20px;\">".$_GET['info']."</h3>";
+                
+        				?>
+       				</form>	
        			</div>
+        		<div id="tlacitko">
+        			<form method="post">
+        				<input type="button" name="reg_window" value="Registrace uživatele" onclick="showDiv()">
+        				<input type="button" name="reg_windows" value="Vytvořit rezervaci" onclick="hello()">
 
-        	
-       			
-
-
+        			</form>
+        			
+        		</div>		
         	</div>
         </div> 
 	</body>
 	</html>
-
-
-
-	
