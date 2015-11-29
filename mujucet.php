08dce0e7951b7725997e886e1cda9b7177573f00
filija <filija.jakub@gmail.com>
2015-11-29 12:27:44 +0100

    Vytvoreni spravy nad vlastnim uctem

--- mujucet.php
+++ mujucet.php
@@ -1,5 +1,25 @@
 <?php
   session_start();
+  require_once("editace.php");
+  if(isset($_POST['zrusit']))
+  {
+    $link=getConnectDb();
+    $id_rezervace=$_POST['id_rezervace'];
+    
+    $zrusit=$_POST['zrusit'];
+    $submit_id=array_keys($zrusit);
+    $submit_id=$submit_id[0];
+    $zrusit=$id_rezervace[$submit_id];
+
+    $result=mysql_query("SELECT id_letenky FROM letenka NATURAL JOIN rezervace WHERE rezervace.id_rezervace='$zrusit'", $link);
+    $row=mysql_fetch_row($result);
+    $idlet=$row[0];
+    
+     mysql_query("DELETE FROM rezervace WHERE id_rezervace='$zrusit'", $link);
+     mysql_query("UPDATE letenka SET pocet_mist=pocet_mist+1 WHERE id_letenky='$idlet'", $link);
+    
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -62,32 +82,40 @@
         <div id="login">
        		<h2>Moje rezervace</h2>
        	</div>
+        <form method="post">
         <table class="data">
             <tr class="head">
                 <td>Odlet z</td>
                 <td>Destinace</td>
-                <td>Sedadlo</td>
                 <td>Datum</td>
                 <td>Čas odletu</td>
+                <td>Třida</td>
                 <td></td>
               </tr>
                 <?php 
                   require_once("editace.php");
+                  $login=$_SESSION['username'];
                   $link=getConnectDb();
-                  $result=mysql_query("SELECT jmeno, prijmeni, adresa, email, telefon FROM uzivatele");
+                  $result=mysql_query("SELECT odkud, destinace, datum_odletu, cas_odletu, trida, id_rezervace FROM letenka NATURAL JOIN 
+                                      rezervace NATURAL JOIN uzivatele WHERE rezervace.id_cestujici=uzivatele.id_cestujici 
+                                      AND rezervace.id_letenky=letenka.id_letenky AND uzivatele.login='$login'", $link);
+                  $i=0;
               while($row = mysql_fetch_row($result))
                 {
                   echo "<tr>";
-                  echo "<td>$row[0] </td>";
-                    echo "<td>$row[1] </td>";
-                    echo "<td>$row[2] </td>";
-                    echo "<td>$row[3] </td>";
-                    echo "<td>$row[4] </td>";
-                    echo "<td><input type=\"button\" value=\"Zrušit\" onclick=\"window.close()\"></td><br>";
+                  echo "<td><input type=\"hidden\" name=\"odkud[$i]\">$row[0] </td>";
+                    echo "<td><input type=\"hidden\" name=\"destinace[$i]\">$row[1] </td>";
+                    echo "<td><input type=\"hidden\" name=\"datum_od[$i]\">$row[2] </td>";
+                    echo "<td><input type=\"hidden\" name=\"cas_od[$i]\">$row[3] </td>";
+                    echo "<td><input type=\"hidden\" name=\"trida[$i]\">$row[4] </td>";
+                    echo "<input type=\"hidden\" name=\"id_rezervace[$i]\" value=\"$row[5]\">";
+                    echo "<td><input type=\"submit\" name=\"zrusit[$i]\" value=\"Zrušit\" onclick=\"window.close()\"></td><br>";
                     echo "</tr>\n";
+                    $i++;
                   }
               ?>
               </table>
+              </form>
 
       </div>
 
@@ -95,6 +123,7 @@
         <div id="login">
           <h2>Můj účet</h2>
         </div>
+        <form action="editace.php" method="post"> 
         <table class="data">
             <tr class="head">
                 <td>Jméno</td>
@@ -107,21 +136,25 @@
                 <?php 
                   require_once("editace.php");
                   $link=getConnectDb();
-                  $result=mysql_query("SELECT jmeno, prijmeni, adresa, email, telefon FROM uzivatele");
-              while($row = mysql_fetch_row($result))
-                {
+                  $login=$_SESSION['username'];
+                    $j=0;
+                  $result=mysql_query("SELECT jmeno, prijmeni, adresa, email, telefon, login 
+                                        FROM uzivatele WHERE login='$login'", $link);
+                  $row=mysql_fetch_row($result);    
+                 
                   echo "<tr>";
-                  echo "<td>$row[0] </td>";
-                    echo "<td>$row[1] </td>";
-                    echo "<td>$row[2] </td>";
-                    echo "<td>$row[3] </td>";
-                    echo "<td>$row[4] </td>";
-                    echo "<td><input type=\"button\" value=\"Upravit\" onclick=\"window.close()\"></td><br>";
+                  echo "<td><input name=\"jmeno[$j]\" type=\"text\" class=\"adminace\" value=\"$row[0]\"></td>";
+              echo "<td><input name=\"prijmeni[$j]\"type=\"text\" class=\"adminace\" value=\"$row[1]\"></td>";
+              echo "<td><input name=\"adresa[$j]\" type=\"text\" class=\"adminace\" value=\"$row[2]\"> </td>";
+              echo "<td><input name=\"email[$j]\" type=\"text\" class=\"adminace\" value=\"$row[3]\"></td>";
+              echo "<td><input name=\"telefon[$j]\" type=\"text\" class=\"adminace\" value=\"$row[4]\"> </td>";
+              echo "<input name=\"login[$y]\" type=\"hidden\" class=\"adminace\" value=\"$row[5]\">";
+                    echo "<td><input type=\"submit\" value=\"Upravit\" name=\"submitmuj[0]\"></td><br>";
                     echo "</tr>\n";
-                  }
+                  
               ?>
               </table>
-
+              </form>
       </div>
     </div>
 
